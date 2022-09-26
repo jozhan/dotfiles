814035226a563f76dbc2ca8b95216261c4bb480f
Zhibin Zhang <25186519+jozhan@users.noreply.github.com>
2022-09-26 13:34:09 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ export GH_USERNAME=jozhan
 docker login -u $GH_USERNAME -p $GH_READ_PACKAGE ghcr.io
 
 go env -w GOPRIVATE='*github.com/github/*'
+
+git config --global user.email "25186519+jozhan@users.noreply.github.com"
